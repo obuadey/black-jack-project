c117ae525e194e8f7f0e7bc298bd58b90b1f3dd8
root <root@DESKTOP-M2D1LCR.localdomain>
2021-11-19 17:44:00 +0000

    modified alwaysStickStrategy in PlayerStrategy file

--- src/main/java/org/example/domains/PlayerStrategy.java
+++ src/main/java/org/example/domains/PlayerStrategy.java
@@ -28,15 +28,12 @@ public class PlayerStrategy {
         }
     }
 
-    public void alwaysStickStrategy(Player player){
-        if (player.getScore() <= 21){
-            player.setStatus(Status.STICK);
-            System.out.println(player.getName() + " requests for stick" );
-        }
-        else {
+    public void alwaysStickStrategy(Player player) {
+        player.setStatus(Status.STICK);
+        if (player.getScore() > 21) {
             player.setStatus(Status.BUSTED);
             bustedPlayers.add(player);
-            System.out.println(player.getName() + " has been busted" );
+            System.out.println(player.getName() + " has been busted");
         }
     }
 
